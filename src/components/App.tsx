e9626bc696d40afcd6f3fea564690b7a62933e7f
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-03-16 09:12:55 +0100

    defined types and set data to state

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { IpokemonList } from "../Types/IpokemonList";
 
 import "../styles/App.css";
 
+const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
 const App = () => {
-  return <div>App</div>;
+  const [pokemon, setPokemon] = useState<IpokemonList>();
+  //fetching pokemon data
+  const fetchData = async (url: string) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    setPokemon(data);
+  };
+
+  useEffect(() => {
+    fetchData(url);
+  }, []);
+  return (
+    <main className="container">
+      <h3>Varun</h3>
+    </main>
+  );
 };
 
 export default App;
