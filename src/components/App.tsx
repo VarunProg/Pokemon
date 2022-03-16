fdab7988b805925cbfaff579df48dc03f85ec89b
VarunProg <89865506+VarunProg@users.noreply.github.com>
2022-03-16 13:47:19 +0100

    pokemon name list rendered

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,40 @@ import React, { useEffect, useState } from "react";
 import { IpokemonList } from "../Types/IpokemonList";
 
 import "../styles/App.css";
+import PokemonData from "./PokemonData";
 
 const url = "https://pokeapi.co/api/v2/pokemon?limit=20";
 const App = () => {
-  const [pokemon, setPokemon] = useState<IpokemonList>();
+  const [pokemons, setPokemons] = useState<IpokemonList>();
   //fetching pokemon data
   const fetchData = async (url: string) => {
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
-    setPokemon(data);
+    setPokemons(data);
   };
 
   useEffect(() => {
     fetchData(url);
   }, []);
+
   return (
     <main className="container">
+      <div className="buttons">
+        <button
+          onClick={() => {
+            //if there is no click simply return else fetchData pokemons.next
+            if (!pokemons?.next) return;
+            fetchData(pokemons.next);
+          }}
+        >
+          Next
+        </button>
+      </div>
       <h3>Varun</h3>
+      {pokemons?.results.map((pokemon) => {
+        return <PokemonData key={pokemon.name} pokemon={pokemon} />;
+      })}
     </main>
   );
 };
---
+++ src/components/PokemonData.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { IpokemonList } from "../Types/IpokemonList";
+
+interface Iprops {
+  //results values which is an array
+  pokemon: IpokemonList["results"][0];
+}
+
+const PokemonData = ({ pokemon }: Iprops) => {
+  return (
+    <div className="poke-info">
+      <h3>{pokemon.name}</h3>
+    </div>
+  );
+};
+
+export default PokemonData;
